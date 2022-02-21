b4b471e63d61abaf8f4c1f107840f657e9ddecf2
Pablo Alonso <99421470+Pablo1734@users.noreply.github.com>
2022-02-21 09:34:36 +0100

    Add files via upload

---
+++ lab_01.py
@@ -0,0 +1,6 @@
+print("I like green birds.")
+print("My mum sings every day at breakfast.")
+print("My mobile is outdated, I need a new one.")
+print("The participants will be: \n-Pep \n-Tommy \n-David")
+
+
